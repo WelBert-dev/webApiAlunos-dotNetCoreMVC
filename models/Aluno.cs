e6de087fa322f8688716f8a61253af06df0dfebd
WelBert-dev <83488863+WelBert-dev@users.noreply.github.com>
2022-07-12 21:33:06 -0300

    Add files via upload

---
+++ models/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webApiAlunos.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]  // define que é um campo obrigatório
+        [StringLength(80)] // define o limite da coluna varchar, poderia utilizar tbm "ErrorMessage = mensagem"
+        public string Nome { get; set; }
+        [EmailAddress] // define que deve ser um email
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        public int Idade { get; set; }
+    }
+}
