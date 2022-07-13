129c8ce0d3c1da3b5d6a4c1f2ce603a0e4244f07
WelBert-dev <83488863+WelBert-dev@users.noreply.github.com>
2022-07-12 21:36:26 -0300

    Add files via upload

---
+++ context/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApiAlunos.Models;
+
+namespace webApiAlunos.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Verifica se tem dados no "Aluno" e se não tiver inclui 2 novos
+            // para atulizar essas infos utilizar: No Nuget: add-migration Nome-Da-Migration (add-migration PopulaTabela)
+            // para rodar a migration criada: update-database
+
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Wellison da Cruz Bertelli",
+                    Email = "wellison.bertelli@hotmail.com",
+                    Idade = 22
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Sonin Bleinin",
+                    Email = "sonin@hotmail.com",
+                    Idade = 69
+                }
+                );
+        }
+    }
+}
